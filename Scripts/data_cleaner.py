df6c6b97453e007202de467430b16f137846a69e
Merrick <merrickneo@gmail.com>
2023-01-30 00:58:11 +0800

    Update data_cleaner.py file.

--- Scripts/data_cleaner.py
+++ Scripts/data_cleaner.py
@@ -6,5 +6,4 @@ df = pd.read_csv("Data/black_honey.csv")
 df = df[df.iloc[:, 0] != "[deleted]"]
 
 # Mine doesn't have emojis surprisingly so i just removed the deleted rows
-
 df.to_csv("Data/black_honey_clean.csv", index=False)
