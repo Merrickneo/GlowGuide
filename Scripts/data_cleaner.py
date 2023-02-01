07edbc47874d9932da8ff88025850f5c2fbf1d27
Merrick <merrickneo@gmail.com>
2023-02-01 10:11:23 +0800

    Update data_cleaner.py file to remove rows with "[removed" data

--- Scripts/data_cleaner.py
+++ Scripts/data_cleaner.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 df = pd.read_csv("Data/black_honey.csv")
 df = df[df.iloc[:, 0] != "[deleted]"]
+df = df[df.iloc[:, 0] != "[removed"]
 
 # Mine doesn't have emojis surprisingly so i just removed the deleted rows
 df.to_csv("Data/black_honey_clean.csv", index=False)
