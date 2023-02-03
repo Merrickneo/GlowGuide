7019eaf7ab133691a71fba50c5aa63862ef42482
Merrick <merrickneo@gmail.com>
2023-02-03 21:55:39 +0800

    Add clean data.

--- Scripts/data_cleaner.py
+++ Scripts/data_cleaner.py
@@ -2,9 +2,9 @@ from emoji_remover import emoji_remover
 
 import pandas as pd
 
-df = pd.read_csv("Data/black_honey.csv")
+df = pd.read_csv("Data/joseon_rice.csv")
 df = df[df.iloc[:, 0] != "[deleted]"]
 df = df[df.iloc[:, 0] != "[removed"]
 
 # Mine doesn't have emojis surprisingly so i just removed the deleted rows
-df.to_csv("Data/black_honey_clean.csv", index=False)
+df.to_csv("Data/joseon_rice.csv", index=False)
